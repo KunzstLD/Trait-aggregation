94b5137ac1e0268c9f3c477e8b2bea4a9b0811cf
Kunzst <sk193@web.de>
2019-12-10 18:50:24 +0100

    Added Hmisc package

--- R/Set_up.R
+++ R/Set_up.R
@@ -4,8 +4,7 @@ library(purrr)
 library(data.table) # data transformation
 library(ggplot2) # plotting
 library(xtable) # convert R output to Latex output
+library(Hmisc)
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
-
-
