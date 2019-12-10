8fc7fb4a59bfc3dba91aaf005fdf735947a90329
Kunzst <sk193@web.de>
2019-12-10 18:49:55 +0100

    Added more detailed comments

--- R/02_comparing_agg_methods.R
+++ R/02_comparing_agg_methods.R
@@ -3,5 +3,15 @@
 # 2) Species - Family
 # TODO: 
   # Provide Details on aggregation methods
-  # Write two functions for aggregation
+  # Write functions for aggregation
   # compare accross regions
+
+# Methods used: 
+  # Aggregation over genus and family (using median and mode)
+  # Aggregation directly to family level
+  # (Aggregation using Power Average Operator?)
+
+# Test for: 
+ # fuzzy coded data
+ # binary coded data
+ # binary data transformed to fuzzy coded (on genus level, by nr. of species)
