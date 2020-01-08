33f5a2097f9765df061df8ee558203eb5b8e290e
Kunzst <sk193@web.de>
2020-01-08 21:41:11 +0100

    Made code more readable

--- R/used_functions.R
+++ R/used_functions.R
@@ -1,19 +1,19 @@
 
 # ___________________________________________________________________________________
-# Get only complete trait data 
+# Get only complete trait data
 # from a dataset with traits and taxa information
-# get those traits that are complete (meaning at least one value different 
+# get those traits that are complete (meaning at least one value different
 # from zero)
 # Gives a list with as many datasets as traits are presented
 # Can be merged together using Reduce!
-# works atm without considering NAs 
+# works atm without considering NAs
 # non trait column need to be manually defined by user
 # ___________________________________________________________________________________
 get_complete_trait_data <- function(trait_data, non_trait_col) {
-  
+
   # pattern of non trait col names
   non_trait_col_pat <- paste0("?(i)", paste0(non_trait_col, collapse = "|"))
-  
+
   # create name vector
   name_vec <-
     grep(
@@ -24,24 +24,27 @@ get_complete_trait_data <- function(trait_data, non_trait_col) {
     ) %>%
     sub("\\_.*", "", .) %>%
     unique()
-  
+
   # create output matrix
   output <- matrix(ncol = 2, nrow = length(name_vec))
-  
+
   data <- list()
   for (i in seq_along(name_vec)) {
     row <- trait_data[, base::sum(.SD) > 0,
-                      .SDcols = names(trait_data) %like% name_vec[i],
-                      by = 1:nrow(trait_data)]$V1
-    
+      .SDcols = names(trait_data) %like% name_vec[i],
+      by = 1:nrow(trait_data)
+    ]$V1
+
     #
     data[[i]] <- trait_data[row, .SD,
-                            .SDcols = names(trait_data) %like%
-                              paste(c(
-                                name_vec[i],
-                                non_trait_col
-                              ),
-                              collapse = "|")]
+      .SDcols = names(trait_data) %like%
+        paste(c(
+          name_vec[i],
+          non_trait_col
+        ),
+        collapse = "|"
+        )
+    ]
     names(data)[[i]] <- name_vec[[i]]
   }
   return(data)
@@ -66,30 +69,33 @@ create_pattern_ind <- function(x, non_trait_cols) {
 # ___________________________________________________________________________________
 #### Normalization of trait scores ####
 # All trait states of one trait are divided by their row sum
-# Hence, trait affinities are represented as "%" or ratios 
+# Hence, trait affinities are represented as "%" or ratios
 # ___________________________________________________________________________________
 normalize_by_rowSum <- function(x, non_trait_cols) {
-  
+
   # get trait names & create pattern for subset
-  trait_names_pattern <- create_pattern_ind(x = x,
-                                            non_trait_cols = non_trait_cols)
-  
+  trait_names_pattern <- create_pattern_ind(
+    x = x,
+    non_trait_cols = non_trait_cols
+  )
+
   # loop for normalization (trait categories for each trait sum up to 1)
   for (cols in trait_names_pattern) {
     # get row sum for a specific trait
     x[, rowSum := apply(.SD, 1, sum), .SDcols = names(x) %like% cols]
-    
+
     # get column names for assignment
     col_name <- names(x)[names(x) %like% cols]
-    
+
     # divide values for each trait state by
     # the sum of trait state values
     x[, (col_name) := lapply(.SD, function(y) {
       round(y / rowSum, digits = 2)
     }),
-    .SDcols = names(x) %like% cols]
+    .SDcols = names(x) %like% cols
+    ]
   }
-  
+
   # del rowSum column
   x[, rowSum := NULL]
   return(x)
@@ -99,24 +105,29 @@ normalize_by_rowSum <- function(x, non_trait_cols) {
 #### check for completeness of trait dataset ####
 # ___________________________________________________________________________________
 completeness_trait_data <- function(x, non_trait_cols) {
-  
-  trait_names_pattern <- create_pattern_ind(x = x, 
-                                            non_trait_cols = non_trait_cols)
-  
+  trait_names_pattern <- create_pattern_ind(
+    x = x,
+    non_trait_cols = non_trait_cols
+  )
+
   # test how complete trait sets are
   output <- matrix(ncol = 2, nrow = length(trait_names_pattern))
   for (i in seq_along(trait_names_pattern)) {
     # vector containing either 0 (no NAs) or a number (> 0) meaning that all
     # entries for this trait contained NA
     vec <-
-      x[, apply(.SD, 1, function(y)
-        base::sum(is.na(y))),
-        .SDcols = names(x) %like% trait_names_pattern[[i]]]
-    
+      x[, apply(.SD, 1, function(y) {
+        base::sum(is.na(y))
+      }),
+      .SDcols = names(x) %like% trait_names_pattern[[i]]
+      ]
+
     # How complete is the dataset for each individual trait?
-    output[i,] <-
-      c((length(vec[vec == 0]) / nrow(x))  %>% `*` (100) %>% round(),
-        trait_names_pattern[[i]])
+    output[i, ] <-
+      c(
+        (length(vec[vec == 0]) / nrow(x)) %>% `*`(100) %>% round(),
+        trait_names_pattern[[i]]
+      )
   }
   return(output)
 }
@@ -127,8 +138,9 @@ completeness_trait_data <- function(x, non_trait_cols) {
 # when there are no duplicate values, mode returns the first value!
 # ___________________________________________________________________________________
 Mode <- function(x, na.rm = FALSE) {
-  if (na.rm)
+  if (na.rm) {
     x <- x[!is.na(x)]
+  }
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
