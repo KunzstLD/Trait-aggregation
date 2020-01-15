2b54b52b1efbac034e6b8cf02c672b1fa3bc4ea9
Kunzst <sk193@web.de>
2020-01-15 15:45:18 +0100

    Added testthat for unit testing

--- R/Set_up.R
+++ R/Set_up.R
@@ -5,6 +5,7 @@ library(data.table) # data transformation
 library(ggplot2) # plotting
 library(xtable) # convert R output to Latex output
 library(Hmisc)
+library(testthat)
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
