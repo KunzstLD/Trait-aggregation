80baa3bed9ee1f94168b59e10d916edd2d768297
Kunzst <sk193@web.de>
2020-01-15 15:47:21 +0100

    Simplified preprocessing

--- R/01_preprocessing_trait_agg.R
+++ R/01_preprocessing_trait_agg.R
@@ -2,48 +2,58 @@
 #### Preprocessing ####
 # _________________________________________________________________________
 
-# read in AST traits
-Trait_AUS <- readRDS(
-  file = file.path(
-    ".",
-    "Data",
-    "Australia",
-    "Trait_AUS_harmonized.rds"
-  )
-)
+trait_dat <- load_data(path = "./Data/", pattern = ".*rds")
+
+preproc_dat <- trait_dat %>%
+  lapply(., function(y) {
+    y[, .SD,
+      .SDcols = names(y) %like% "species|genus|family|order|feed.+|locom.+|size.+|volt.+|resp.+"
+    ] %>%
+      normalize_by_rowSum(.,
+        non_trait_cols = c(
+          "species",
+          "genus",
+          "family",
+          "order"
+        )
+      ) %>%
+      na.omit(., cols = names(.[, -c(
+        "species",
+        "genus",
+        "family",
+        "order"
+      )]))
+  })
+
+# AUS_subset <- Trait_AUS %>%
+#   .[, .SD, .SDcols = names(.) %like% "species|genus|family|order|
+#   feed.+|locom.+|size.+|volt.+|resp.+"] %>%
+#   normalize_by_rowSum(.,
+#     non_trait_cols = c(
+#       "species",
+#       "genus",
+#       "family",
+#       "order"
+#     )
+#   ) %>%
+#   na.omit(., cols = names(.[, -c(
+#     "species",
+#     "genus",
+#     "family",
+#     "order"
+#   )]))
+
+
+# # read in AST traits
+# Trait_AUS <- readRDS(
+#   file = file.path(
+#     ".",
+#     "Data",
+#     "Trait_AUS_harmonized.rds"
+#   )
+# )
 
 # search for candidate families
-Trait_AUS[, .(.N), by = family] %>%
-  .[order(N), ] %>%
-  tail(., n = 20)
-
-# create subset
-candidates <- c(
-  "Chironomidae",
-  "Elmidae",
-  "Dytiscidae",
-  "Ceratopogonidae",
-  "Tipulidae"
-)
-
-AUS_subset <- Trait_AUS[!is.na(genus), ] %>%
-  melt(., id.vars = c("unique_id", "species", "genus", "family", "order")) %>%
-  .[variable %like% "feed.+|locom.+|size.+|volt.+|resp.+", ] %>%
-  .[family %in% candidates, ] %>%
-  dcast(.,
-        unique_id + species + genus + family + order ~ variable,
-        value.var = "value") %>%
-  normalize_by_rowSum(.,
-                      non_trait_cols = c("unique_id",
-                                         "species",
-                                         "genus",
-                                         "family",
-                                         "order")) %>%
-  na.omit(., cols = names(.[, -c("unique_id",
-                                 "species",
-                                 "genus",
-                                 "family",
-                                 "order")]))
-
-# remove unique_id
-AUS_subset[, unique_id := NULL]
+# Trait_AUS[, .(.N), by = family] %>%
+#   .[order(N), ] %>%
+#   tail(., n = 20)
