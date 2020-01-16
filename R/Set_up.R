f267704c2aae989d2237d25d255d9d73a7a010f4
Kunzst <sk193@web.de>
2020-01-16 16:30:36 +0100

    Added few packages

--- R/Set_up.R
+++ R/Set_up.R
@@ -1,11 +1,12 @@
 # libraries
+library(readxl) # reading excel files
 library(dplyr) # basic data transformation
-library(purrr)
+library(purrr) # data transformation
 library(data.table) # data transformation
 library(ggplot2) # plotting
 library(xtable) # convert R output to Latex output
-library(Hmisc)
-library(testthat)
+library(Hmisc) # data summaries
+library(testthat) # unit testing
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
