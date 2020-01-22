ad5799ae94fc737519fa37bd721d141b41726176
Kunzst <sk193@web.de>
2020-01-22 18:52:06 +0100

    Rm commented code

--- R/01_preprocessing_trait_agg.R
+++ R/01_preprocessing_trait_agg.R
@@ -24,36 +24,3 @@ preproc_dat <- trait_dat %>%
         "order"
       )]))
   })
-
-# AUS_subset <- Trait_AUS %>%
-#   .[, .SD, .SDcols = names(.) %like% "species|genus|family|order|
-#   feed.+|locom.+|size.+|volt.+|resp.+"] %>%
-#   normalize_by_rowSum(.,
-#     non_trait_cols = c(
-#       "species",
-#       "genus",
-#       "family",
-#       "order"
-#     )
-#   ) %>%
-#   na.omit(., cols = names(.[, -c(
-#     "species",
-#     "genus",
-#     "family",
-#     "order"
-#   )]))
-
-
-# # read in AST traits
-# Trait_AUS <- readRDS(
-#   file = file.path(
-#     ".",
-#     "Data",
-#     "Trait_AUS_harmonized.rds"
-#   )
-# )
-
-# search for candidate families
-# Trait_AUS[, .(.N), by = family] %>%
-#   .[order(N), ] %>%
-#   tail(., n = 20)
