acf78f185f389c7b73bd792d0459e615f060682a
Kunzst <sk193@web.de>
2020-01-27 15:59:42 +0100

    Added few packages for plotting

--- R/Set_up.R
+++ R/Set_up.R
@@ -1,12 +1,13 @@
 # libraries
-library(readxl) # reading excel files
 library(dplyr) # basic data transformation
-library(purrr) # data transformation
+library(purrr)
 library(data.table) # data transformation
 library(ggplot2) # plotting
+library(ggsci) # plotting 
+library(cowplot) # plot arangemeng
 library(xtable) # convert R output to Latex output
-library(Hmisc) # data summaries
-library(testthat) # unit testing
+library(Hmisc)
+library(readxl)
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
