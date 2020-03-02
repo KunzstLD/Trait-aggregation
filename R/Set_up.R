8f7f0c0e4317ca4b1edd7bef51660b3cae07215c
Kunzst <sk193@web.de>
2020-03-02 11:33:20 +0100

    Slight changes

--- R/Set_up.R
+++ R/Set_up.R
@@ -3,11 +3,12 @@ library(dplyr) # basic data transformation
 library(purrr)
 library(data.table) # data transformation
 library(ggplot2) # plotting
-library(ggsci) # plotting 
-library(cowplot) # plot arangemeng
+library(ggsci) # plotting
+library(cowplot) # arranging plots
 library(xtable) # convert R output to Latex output
 library(Hmisc)
 library(readxl)
+library(forcats)
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
