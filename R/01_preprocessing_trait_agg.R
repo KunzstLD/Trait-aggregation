324a4d678549198a5233f0629eca0bec94259f3e
Kunzst <sk193@web.de>
2020-03-02 11:26:05 +0100

    Improved names in list

--- R/01_preprocessing_trait_agg.R
+++ R/01_preprocessing_trait_agg.R
@@ -1,14 +1,13 @@
 # _________________________________________________________________________
 #### Preprocessing ####
 # _________________________________________________________________________
-
-trait_dat <- load_data(path = "./Data/", pattern = "harmonized\\.rds")
+trait_dat <- load_data(path = "./Data/", pattern = "harmonized.*\\.rds")
 
 # choose traits, normalize and omit incomplete information
 preproc_dat <- trait_dat %>%
   lapply(., function(y) {
     y[, .SD,
-      .SDcols = names(y) %like% "species|genus|family|order|feed.+|locom.+|size.+|volt.+|resp.+"
+      .SDcols = names(y) %like% "species|genus|family|order|feed.+|locom.+|size.+|volt.+|resp.+|bf.+"
     ] %>%
       normalize_by_rowSum(.,
         non_trait_cols = c(
@@ -25,3 +24,6 @@ preproc_dat <- trait_dat %>%
         "order"
       )]))
   })
+
+# improve names
+names(preproc_dat) <- sub("(.+)(\\.rds)", "\\1", names(preproc_dat))
