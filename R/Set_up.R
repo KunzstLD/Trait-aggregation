ba773d861cd5f56cf5119f0d674c166dd67b6864
Kunzst <sk193@web.de>
2020-04-27 19:53:24 +0200

    Structured script

--- R/Set_up.R
+++ R/Set_up.R
@@ -1,19 +1,33 @@
 # libraries
-library(dplyr) # basic data transformation
-library(purrr)
-library(data.table) # data transformation
-library(ggplot2) # plotting
-library(ggsci) # plotting
-library(cowplot) # arranging plots
-library(xtable) # convert R output to Latex output
-library(Hmisc)
-library(readxl)
+
+# data transformation & data handling
+library(dplyr) 
+library(data.table)
+library(broom)
 library(forcats)
-library(testthat) # unit tests
-library(plotly) # interactive graphics
-library(zeallot) # %<-% operator
-library(ade4) # trait analysis
-library(vegan) # trait analysis
+library(purrr)
+library(readxl)
+library(zeallot)
+
+# plotting & arranging plots
+library(ggplot2) 
+library(ggsci) # plotting
+library(cowplot)  
+library(plotly)
+
+# data summaries
+library(Hmisc)
+library(gtsummary)
+
+# unit tests
+library(testthat) 
+
+# trait analysis
+library(ade4) 
+library(vegan) 
+
+# convert R output to Latex output
+library(xtable) 
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
