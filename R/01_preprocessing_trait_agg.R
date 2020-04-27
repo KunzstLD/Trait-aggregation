0d0d9a2f90502cbff8def64accac933a334d28c4
Kunzst <sk193@web.de>
2020-04-27 19:49:06 +0200

    Added workaround for feeding mode parasite

--- R/01_preprocessing_trait_agg.R
+++ R/01_preprocessing_trait_agg.R
@@ -1,13 +1,16 @@
 # _________________________________________________________________________
 #### Preprocessing ####
+# TODO: investigate fuzzy coded version as well
 # _________________________________________________________________________
-trait_dat <- load_data(path = "./Data/", pattern = "harmonized.*\\.rds")
+trait_dat <- load_data(path = "./Data/", pattern = "harmonized\\.rds")
 
-# choose traits, normalize and omit incomplete information
+# chose traits, normalize and omit incomplete information
+# for trait aggregation comparison
+# TODO: Only choose traits with sufficient coverage?
 preproc_dat <- trait_dat %>%
   lapply(., function(y) {
     y[, .SD,
-      .SDcols = names(y) %like% "species|genus|family|order|feed.+|locom.+|size.+|volt.+|resp.+|bf.+"
+      .SDcols = names(y) %like% "species|genus|family|order|feed.+|resp.+|locom.+|bf.+|volt.+|size.+|ovip.+"
     ] %>%
       normalize_by_rowSum(.,
         non_trait_cols = c(
@@ -28,3 +31,21 @@ preproc_dat <- trait_dat %>%
 
 # improve names
 names(preproc_dat) <- sub("(.+)(\\.rds)", "\\1", names(preproc_dat))
+
+# feeding mode parasite:
+# remove trait and normalize again
+lapply(preproc_dat[c("Trait_AUS_harmonized",
+                   "Trait_EU_pp_harmonized",
+                   "Traits_US_LauraT_pp_harmonized")],
+       function(y) y[feed_parasite > 0, ])
+preproc_dat$Trait_EU_pp_harmonized[,  feed_parasite := NULL]
+preproc_dat$Trait_AUS_harmonized[,  feed_parasite := NULL]
+preproc_dat$Traits_US_LauraT_pp_harmonized[,  feed_parasite := NULL]
+
+# normalize Europe data again
+normalize_by_rowSum(preproc_dat$Trait_EU_pp_harmonized, 
+                    non_trait_cols = c(
+                      "species",
+                      "genus",
+                      "family",
+                      "order"))
