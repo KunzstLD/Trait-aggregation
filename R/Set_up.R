0c1aefa61ffb29afff9bdeffe4816fca1f347ae3
KunzstLD <sk193@web.de>
2020-10-07 17:26:59 +0200

    07.10.: Added rtruncnorm

--- R/Set_up.R
+++ R/Set_up.R
@@ -1,8 +1,8 @@
 # libraries
-
 # data transformation & data handling
 library(dplyr) 
 library(data.table)
+library(tidyr)
 library(broom)
 library(forcats)
 library(purrr)
@@ -11,26 +11,32 @@ library(zeallot)
 
 # plotting & arranging plots
 library(ggplot2) 
-library(ggsci) # plotting
+library(ggsci) 
 library(cowplot)  
-library(plotly)
+# library(plotly)
 
 # data summaries
 library(Hmisc)
-library(gtsummary)
+# library(gtsummary)
 
 # unit tests
 library(testthat) 
 
 # trait analysis
-library(ade4) 
-library(vegan) 
+# library(ade4) 
+# library(vegan) 
 
 # convert R output to Latex output
-library(xtable) 
+# library(xtable) 
+
+# simulation
+library(truncnorm)
 
 # source script with used functions
 source(file = file.path(".", "R", "used_functions.R"))
 
 # data output path
 data_out <- "./Output"
+
+# output for paper
+data_paper <- "/home/kunz/Dokumente/Projects/Trait_DB/Invertebrate_traits/Paper/Figures"
