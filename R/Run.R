d1b6e84afd0c6c77f346647f4c0b5b0165376d85
KunzstLD <sk193@web.de>
2020-10-16 23:50:14 +0200

    16.10: Add script to run analyses

---
+++ R/Run.R
@@ -0,0 +1,11 @@
+#### Scripts to run analysis ####
+
+# Packages, paths and functions
+source("./R/Set_up.R")
+
+# preprocessing comparison of trait aggregation
+# methods
+source("./R/01_preprocessing_trait_agg.R")
+
+# preprocessing for re-analysis of Szöcs et al
+source("./R/Reanalysis_salinization/01_data_processing.R")
